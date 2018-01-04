e51f84a134a2d675963ba7fbbdd1dbd5a00856d8
dana <raheldana50@gmail.com>
2018-01-04 20:45:40 +0200

    classwork

--- addressbook-test/src/test/java/tests/DeleteGroup.java
+++ addressbook-test/src/test/java/tests/DeleteGroup.java
@@ -1,23 +1,22 @@
+package tests;
+
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DeleteGroup extends TestBase {
     @Test
     public void deleteGroup() {
         openGroupPage();
+        int before = wd.findElements(By.name("selected[]")).size();
         //selectGroupTodelete
         selectGroupTodelete();
         //submitGroupDelete
         submitGroupDelete();
         //returnToGroupPage
         returnToGroupPage();
+        int after = wd.findElements(By.name("selected[]")).size();
+        Assert.assertEquals(before, after+1);
     }
 
-    public void submitGroupDelete() {
-        wd.findElement(By.xpath("//input[@name='delete']")).click();
-    }
-
-    public void selectGroupTodelete() {
-        wd.findElement(By.xpath("//input[@type='checkbox']")).click();
-    }
 }
