f5dd8760c568c76297672ff2f3d40e23debe8b44
MalykhinaA <malykhinaleksandra@gmail.com>
2018-01-06 15:08:41 +0400

    homework06.01

--- addressbook-test/src/test/java/tests/CreateContact.java
+++ addressbook-test/src/test/java/tests/CreateContact.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import org.openqa.selenium.*;
@@ -8,12 +9,15 @@ public class CreateContact extends TestBase {
 
     @Test
     public void createContact() {
+        int before = wd.findElements(By.xpath("//*[@title='vCard']")).size();
         //initCreationContact
         initCreationContact();
         //fillContactForm
         fillContactForm("Name", "Surname", "1970");
         //submitCreationContact
         submitCreateContact();
+        int after = wd.findElements(By.xpath("//*[@title='vCard']")).size();
+        Assert.assertEquals(before+1, after);
     }
 
 }
